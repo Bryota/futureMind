e924a6eb862766466b56fc734ccc9b0c0df3f7c4
sgw-ryota <ryota-segawe@sekawamshinoMBP.lan>
2022-03-21 20:13:15 +0900

    add diagnosis questions import

--- app/Http/Controllers/Admin/ImportCSVController.php
+++ app/Http/Controllers/Admin/ImportCSVController.php
@@ -19,6 +19,11 @@ class ImportCSVController extends Controller
         return view('admin.importCSV.import_diagnosis_question');
     }
 
+    /**
+     * 診断質問追加
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function store(Request $request)
     {
         $file = $request->file('file');
